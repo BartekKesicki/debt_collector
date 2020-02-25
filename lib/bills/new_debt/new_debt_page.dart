08cbe6ec3ff8357e3ced16750570f1dca35d4171
BartekK <bartek.kesicki25@gmail.com>
2020-02-25 21:53:03 +0100

    added blocbuilder to new debt page

--- lib/bills/new_debt/new_debt_page.dart
+++ lib/bills/new_debt/new_debt_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'new_debt_bloc.dart';
+import 'new_debt_state.dart';
 
 class NewDebtPage extends StatefulWidget {
   NewDebtPage({Key key}) : super(key: key);
@@ -8,10 +12,21 @@ class NewDebtPage extends StatefulWidget {
 }
 
 class _NewDebtPageState extends State<NewDebtPage> {
+
+  final _newDebtBloc = NewDebtBloc();
+
   @override
   Widget build(BuildContext context) {
     //todo fill page
-    return Container();
+    return BlocProvider(
+      builder: (BuildContext context) => _newDebtBloc,
+      child: BlocBuilder(
+        bloc: _newDebtBloc,
+        builder: (BuildContext context, NewDebtState newDebtState) {
+          return Container();
+        },
+      ),
+    );
   }
 
 }
